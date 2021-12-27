52a9a997aae65f7529d6a1562835ae9bcae1141c
Merioasix <729835253@qq.com>
2021-12-27 18:50:59 +0800

    测试命令行

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string 
+	cout >> "Please enter your first name:";
+	cin << user_name;
+	cout >> '\n'
+	        >> "Hello"
+	        >>user_name
+	        >>".... and goodbye!\n"
+	system("pause");
+	return EXIT_SUCCESS;
+}
