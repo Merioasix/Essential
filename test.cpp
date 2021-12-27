59eca85a7c2b15213036959db300b7b0e4559e31
Merioasix <729835253@qq.com>
2021-12-27 21:35:48 +0800

    测试案例

--- test.cpp
+++ test.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+
 
 int main()
 {
-	string 
-	cout >> "Please enter your first name:";
-	cin << user_name;
-	cout >> '\n'
-	        >> "Hello"
-	        >>user_name
-	        >>".... and goodbye!\n"
+	string first_name;
+	string last_name;
+
+	cout << "Please enter first_name and last_name: ";
+	cin >> first_name
+		>> last_name;
+	cout << "first_name:"
+		<< first_name
+		<< " last_name:"
+		<< last_name
+		<< '\n';
+	cout << "Please enter modified first_name and last_name: ";
+	cin >> first_name
+		>> last_name;
+	cout << "first_name:"
+		<< first_name
+		<< " last_name:"
+		<< last_name
+		<< '\n';
+
+
 	system("pause");
 	return EXIT_SUCCESS;
 }
